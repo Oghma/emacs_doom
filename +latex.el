1ee306a28ad9bbcfb1dbdb22fe7a29f2a029f820
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-07-20 10:52:28 +0200

    Add hunspell it vocabulary

--- +latex.el
+++ +latex.el
@@ -1,3 +1,6 @@
 ;;; ~/.doom.d/+latex.el -*- lexical-binding: t; -*-
 
-(setq ispell-dictionary "italiano")
+(setq ispell-program-name "hunspell")
+(setq ispell-local-dictionary "it_IT")
+(setq ispell-local-dictionary-alist
+      '(("it_IT" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8)))
