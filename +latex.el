36629b870332035909f57a7bbe7cb4c5876b1719
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-07-27 17:47:43 +0200

    Add multiple dictionary for ispell

--- +latex.el
+++ +latex.el
@@ -3,4 +3,4 @@
 (setq ispell-program-name "hunspell")
 (setq ispell-local-dictionary "it_IT")
 (setq ispell-local-dictionary-alist
-      '(("it_IT" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8)))
+      '(("it_IT" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "it_IT,en_US") nil utf-8)))
