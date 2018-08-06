869c06e20f2c1fc93b2fbcfe2076ee1a310c1308
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-08-06 11:25:05 +0200

    Try helm instead of ivy

--- init.el
+++ init.el
@@ -24,11 +24,12 @@
        (company          ; the ultimate code completion backend
         +auto            ; as-you-type code completion
         +childframe)     ; a nicer company UI (Emacs 26+ only)
-      ;(helm             ; the *other* search engine for love and life
+       (helm             ; the *other* search engine for love and life
+        +childframe)     ; uses childframes for popups (Emacs 26+ only)
       ; +fuzzy)          ; enable fuzzy search backend for helm
       ;ido               ; the other *other* search engine...
-       (ivy              ; a search engine for love and life
-        +childframe)     ; uses childframes for popups (Emacs 26+ only)
+      ;(ivy              ; a search engine for love and life
+        ;+childframe)    ; uses childframes for popups (Emacs 26+ only)
         ;+fuzzy          ; enable fuzzy search backend for ivy
 
        :ui
