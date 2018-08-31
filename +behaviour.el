5014c8b21389a466fd6cce573d8a86396ebc9d9f
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-08-31 13:33:33 +0200

    Trailing whitespace is default in doom

--- +behaviour.el
+++
@@ -1,5 +0,0 @@
-;;; ~/.doom.d/+behaviour.el -*- lexical-binding: t; -*-
-
-;; Hook to remove the wthitespace
-(add-hook 'before-save-hook 'whitespace-cleanup)
-
--- config.el
+++ config.el
@@ -1,13 +1,12 @@
 ;;; config.el --- description -*- lexical-binding: t; -*-
 
 (when IS-MAC
-    (load! "+mac"))
+  (load! "+mac"))
 
 (load! "+ui")
 (load! "+bindings")
 (load! "+mail")
 (load! "+org.el")
-(load! "+behaviour.el")
 (load! "+calendar.el")
 (load! "+completion.el")
 (load! "+latex.el")
