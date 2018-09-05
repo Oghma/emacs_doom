234cdf3b72a3ce33b672fe1aeb8b5f292f16deb0
Matteo Lisotto <matteo.lisotto@gmail.com>
2018-09-05 23:15:53 +0200

    Use python3 instead of python2

--- +mac.el
+++ +mac.el
@@ -1,2 +1,8 @@
 ;;; +mac.el -*- lexical-binding: t; -*-
 (exec-path-from-shell-initialize)
+
+(after! python
+  (setq python-shell-interpreter "python3"))
+
+(after! anaconda-mode
+  (setq python-shell-interpreter "ipython3"))
