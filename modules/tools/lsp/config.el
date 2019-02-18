26274db8f881abeacc188b9e4217ab4233c80831
Matteo Lisotto <matteo.lisotto@gmail.com>
2019-02-18 14:50:02 +0100

    Update lsp config

--- modules/tools/lsp/config.el
+++ modules/tools/lsp/config.el
@@ -3,17 +3,21 @@
 (use-package lsp-mode
   :commands lsp
   :config
-  (setq lsp-auto-guess-root t)
-  (add-hook! 'python-mode-hook #'lsp))
+  (setq lsp-prefer-flymake nil))
 
 (def-package! lsp-ui
   :hook (lsp-mode . lsp-ui-mode)
-  :commands lsp-ui-mode)
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-include-signature t))
 
 (def-package! company-lsp
   :when (featurep! :completion company)
-  :commands company-lsp)
+  :commands company-lsp
+  :after lsp-mode
+  :config
+  (set-company-backend! 'lsp-mode 'company-lsp))
 
 ;; For now only python
 (when (featurep! :tools lsp)
-  (add-hook 'python-mode-hook #'lsp))
+  (add-hook! 'python-mode-hook #'lsp))
