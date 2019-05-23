4fb0af8bee1abac20d07e1c1de42ac12b9032b72
Matteo Lisotto <matteo.lisotto@gmail.com>
2019-05-23 23:03:02 +0200

    Add lsp-python-ms

--- +lsp.el
+++ +lsp.el
@@ -1,7 +1,14 @@
 ;;; ~/.doom.d/+lsp.el -*- lexical-binding: t; -*-
 
-(after! lsp-mode
-  (setq lsp-prefer-flymake nil))
-
 (after! lsp-ui
  (setq lsp-ui-doc-include-signature t))
+
+(def-package! lsp-python-ms
+  :after lsp
+  :config
+
+  ;;(remhash 'pyls lsp-clients)
+  (setq lsp-python-ms-dir
+        (expand-file-name "~/.python-language-server/output/bin/Release/"))
+  (setq lsp-python-ms-executable
+        "~/.python-language-server/output/bin/Release/linux-x64/publish/Microsoft.Python.LanguageServer"))
--- packages.el
+++ packages.el
@@ -2,3 +2,4 @@
 ;;; ~/.doom.d/packages.el
 
 (packages! org-super-agenda)
+(package! lsp-python-ms)
